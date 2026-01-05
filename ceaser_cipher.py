1c099921e56cb2fde3501c28b0422c501a28f73a
allForOne-alt <weeesdom1@gmail.com>
2026-01-05 16:47:48 +0545

    doesnot include the file encrypter

---
+++ ceaser_cipher.py
@@ -0,0 +1,62 @@
+def display_welcome():
+    print(" ")
+    print("------Welcome to THE DECIPHER-er------")
+
+def encryption_decryption():
+    while True:
+        print(" ")
+        ch = input("Encrypt(e) or Decrypt(d) or exit to exit : ").lower()
+        
+        if(ch == 'exit'):
+            print("------Thanks for using THE DECIPHER-er------")
+            break
+            
+        elif(ch == 'e'):
+            text = input("Enter text : ").lower()
+            shift = int(input("Shift by how much : "))
+            encrypted_text = []
+            
+            for ch in text:
+                '''
+                so when the user enters somethinglike 'z' just adding the shift value to it i.e 122(z) + 3(shift value) 
+                doesnot give an alphabet it rather gives '{' which is not what I want. So in order to fix this, I first
+                converted the letter into an alphabet number/index then '%26' makes sure that the value that is shifted
+                stays around tha alphabet. So basically, z + 3 gives c. 
+                '''
+                
+                letter_to_number = ord(ch)
+                
+                if letter_to_number >= 97 and letter_to_number <=122:
+                    base = 97
+                    setter = letter_to_number - base
+                    setter = setter + shift
+                    encrypted_text.append(chr(base + setter % 26))
+                
+                else:
+                    encrypted_text.append(ch)
+            
+            for ch in encrypted_text:
+                print(ch.upper(), end="")
+            
+        elif(ch == 'd'):
+            text = input("Enter text to decipher : ").lower()
+            shift = int(input("Shift by how much : "))
+            decrypted_text = [] 
+            
+            for ch in text:
+                letter_to_number = ord(ch)
+                
+                if letter_to_number >= 97 and letter_to_number <=122:
+                    base = 97
+                    setter = letter_to_number - base
+                    setter = setter - shift
+                    decrypted_text.append(chr(base + setter % 26))
+                
+                else:
+                    decrypted_text.append(ch)
+
+            for ch in decrypted_text:
+                print(ch.upper(), end = "")
+
+display_welcome()
+encryption_decryption()
